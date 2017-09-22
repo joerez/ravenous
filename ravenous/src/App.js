89149c46e5d068ea9ae2aa4a7e543c5e4dc8cdbb
Joe <rezenbiz@gmail.com>
2017-09-21 21:02:56 -0700

    app.js changes

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  <h1>ravenous</h1>
+    <SearchBar />
+    <BusinessList />
+</div>
     );
   }
 }
